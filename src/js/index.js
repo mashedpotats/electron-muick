0662053c6da60d8212738d67b03b7bc096634969
mashedpotats <samuelli4521@gmail.com>
2016-10-15 22:37:33 -0700

    finished settings and started content

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,56 @@
 /**
  * Created by samuel on 10/10/16.
  */
-window.addEventListener("load", function () {
 
-    const fs = require("fs");
+const fs = require("fs");
+const $ = require("jquery");
+
+const electron = require('electron').remote;
+const mainWindow = electron.getCurrentWindow();
+var dialog = electron.dialog;
+
+window.addEventListener("load", function () {
+    var $musicDirectoryInput = $("#music-directory-input");
+    var musicDirectory = "";
 
     fs.readFile(__dirname + "/data/audiodir.txt", "utf-8", function (err, data) {
         if (err) {
             throw err;
         }
 
-        document.querySelector(".loading").classList.add("ready");
+        $musicDirectoryInput.val(data);
+
+        if (data != "")
+            $("#audio").attr("src", data + "/foo.mp3");
+        $(".loading").toggleClass("ready");
+    });
+
+    $(".settings-button").on("click", function() {
+        $(".settings").toggleClass("open");
+    });
+
+    $(".settings-shade").on("click", function() {
+        $(".settings").toggleClass("open");
+    });
+
+    function changeDirectory() {
+        if (fs.existsSync($musicDirectoryInput.val())) {
+            musicDirectory = $musicDirectoryInput.val();
+            fs.writeFileSync(__dirname + "/data/audiodir.txt", musicDirectory, "utf-8");
+        } else
+            dialog.showMessageBox(mainWindow, {
+                type: "error",
+                title: "Invalid Folder",
+                message: "You have chose an invalid folder. Please choose another one",
+                buttons: ["Ok"]
+            });
+    }
+
+    $("#music-directory-button").on("click", changeDirectory);
+
+    $(".folder-button").on("click", function () {
+        $musicDirectoryInput.val(dialog.showOpenDialog(mainWindow, {
+            properties: ['openDirectory']
+        })[0]);
     });
 });
